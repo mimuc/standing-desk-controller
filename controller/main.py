85990137c8172d522a7d63f2b8631261dcd6c0ca
lukehali <haliburton.luke@gmail.com>
2022-07-20 19:27:36 +0200

    add custom watchdog and wakeup ping

--- controller/main.py
+++ controller/main.py
@@ -5,15 +5,21 @@
 # At LMU Munich, 2022
 
 import network, socket, time
-from machine import UART, Pin, reset
+from machine import UART, Pin
+import machine
 import ubinascii
 import urequests as requests
 import json
-# import gc
+import gc
+from sys import platform
 
 
-# gc.collect()
-# wdt = WDT(timeout = 10000) # watchdog with 10s timeout
+gc.collect()
+
+# Set Device Name and Version Number
+versionNumber = "0.03"
+mac = "Ares"
+print("mac address: ", mac)
 
 
 # Hard coded wifi name and password
@@ -43,8 +49,10 @@ wlan = connectToWifi(ssid, passwd)
 
 # IPv4 address
 # Change this based on the address of the server
-serverUrl_post = 'http://desks.medien.ifi.lmu.de/heights/add'
-serverUrl_get = 'http://desks.medien.ifi.lmu.de/commands/mac'
+serverUrl_postAlive = 'http://desks.medien.ifi.lmu.de/startupLogger/add'
+serverUrl_postHeight = 'http://desks.medien.ifi.lmu.de/heights/add'
+
+serverUrl_getCommand = 'http://desks.medien.ifi.lmu.de/commands/mac'
 
 
 # These pins correspond with the custom PCB to control the desk
@@ -67,16 +75,15 @@ d3.value(0)
 Rx = bytearray(8)
 uart = UART(0, baudrate=9600, bits=8, parity=None, stop=1)                         
 
-# get macaddress to identify the device - remove the last set ':XX' because it often changes
-mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()[:-3] 
-print("mac address: ", mac)
+
+
 
 
 
 # This function interprets a command fetched from the server and activates the
 # indicated button. For now, this only works with buttons 1-4, the preset height buttons
 def execute_command(newCommand):
-    print('New command: ', newCommand)
+#     print('New command: ', newCommand)
     if newCommand:
         if (newCommand <=4):
             # simulate a button press (1 through 4)
@@ -84,40 +91,40 @@ def execute_command(newCommand):
                 print('Virtually pressed button 1')
                 d0.value(1)
                 d1.value(1)
-                time.sleep(5)
+                time.sleep(15)
                 d0.value(0)
                 d1.value(0)
             elif(newCommand == 2):
                 print('Virtually pressed button 2')
                 d2.value(1)
-                time.sleep(5)
+                time.sleep(15)
                 d2.value(0)
             elif(newCommand == 3):
                 print('Virtually pressed button 3')
                 d1.value(1)
                 d2.value(1)
-                time.sleep(5)
+                time.sleep(15)
                 d1.value(0)
                 d2.value(0)
             elif(newCommand == 4):
                 print('Virtually pressed button 4')
                 d0.value(1)
                 d2.value(1)
-                time.sleep(5)
+                time.sleep(15)
                 d0.value(0)
                 d2.value(0)
             
         else:
             # go to a specific height and then stop
             print('New command is not a preset: ', newCommand)
-    else:
-        print('No commands')
+#     else:
+#         print('No commands')
     
 
 # This function posts a request to the server. The request "req_data"
 # should be in json format
-def post_request(req_data):
-    global serverUrl_post
+def post_request(req_data, serverUrl_post=serverUrl_postHeight):
+    serverUrl_post
     global wlan
     header = {'Content-Type': 'application/json; charset=utf-8'}
     try:
@@ -128,20 +135,15 @@ def post_request(req_data):
         return True
     except Exception as PostException:
         print('POST exception: ', PostException)
-        if PostException == -1:
-            print('POST reset time bitches')
-            reset()
-        # in case the wifi disconnects, try reconnecting
-        if not wlan.isconnected():
-            wlan = connectToWifi(ssid, passwd)
-        return False
+        machine.reset()
+        
     
 
 
 # This function fetches a request from the server. The request "req_data"
 # should be in json format
-def get_request(req_data):
-    global serverUrl_get
+def get_request(req_data, serverUrl_get=serverUrl_getCommand):
+    serverUrl_get
     global wlan
     header = {'Content-Type': 'application/json; charset=utf-8'}
     try:
@@ -153,24 +155,74 @@ def get_request(req_data):
             execute_command(rJson['command'])
     except Exception as GetException:
         print('GET exception: ', GetException)
-        if GetException == -1:
-            print('GET reset time bitches')
-            reset()
-        # in case the wifi disconnects, try reconnecting
-        if not wlan.isconnected():
-            wlan = connectToWifi(ssid, passwd)
+        machine.reset()
     
 
-        
+class WDT:
+    def __init__(self, timeout=120, use_rtc_memory=True):
+        self._timeout = timeout / 10
+        self._counter = 0
+        self._timer = machine.Timer()
+        self._use_rtc_memory = use_rtc_memory
+        self.init()
+        try:
+            with open("watchdog.txt", "r") as f:
+                if f.read() == "True":
+                    print("Reset reason: Watchdog")
+        except Exception as e:
+            print(e)  # file probably just does not exist
+        try:
+            with open("watchdog.txt", "w") as f:
+                f.write("False")
+        except Exception as e:
+            print("Error saving to file: {!s}".format(e))
+            if use_rtc_memory and platform == "rp2":
+                rtc = machine.RTC()
+                if rtc.memory() == b"WDT reset":
+                    print("Reset reason: Watchdog")
+                rtc.memory(b"")
+
+    def _wdt(self, t):
+        self._counter += self._timeout
+        if self._counter >= self._timeout * 10:
+            try:
+                with open("watchdog.txt", "w") as f:
+                    f.write("True")
+            except Exception as e:
+                print("Error saving to file: {!s}".format(e))
+                if self._use_rtc_memory and platform == "esp8266":
+                    rtc = machine.RTC()
+                    rtc.memory(b"WDT reset")
+            machine.reset()
+
+    def feed(self):
+        self._counter = 0
+
+    def init(self, timeout=None):
+        timeout = timeout or self._timeout
+        self._timeout = timeout
+        self._timer.init(period=int(self._timeout * 1000), mode=machine.Timer.PERIODIC, callback=self._wdt)
+
+    def deinit(self):  # will not stop coroutine
+        self._timer.deinit()   
 
 last_time_get = time.time()
 buffer =[]
 lastHeight = -1
 
+# ping the server on startup to say that this unit has just started
+initial_post = {"macaddress": mac, "versionNumber": versionNumber}
+response = post_request(initial_post, serverUrl_postAlive)
+
+# Start the watchdog
+wdt=WDT()
+
 # main loop to deal with  http requests
 while True:
     try:
-#         wdt.feed()
+        # feed the watchdog
+        wdt.feed()
+        
         # read all available characters
         uart.readinto(Rx)         
         flag1=0
@@ -233,18 +285,14 @@ while True:
             get_data = {"macaddress": mac}
             get_request(get_data)
             last_time_get = time.time()
-            print('Time: ', last_time_get)
+#             print('Time: ', last_time_get)
         
         # in case the wifi disconnects, try reconnecting
         if not wlan.isconnected():
             wlan = connectToWifi(ssid, passwd)
     except Exception as MainException:
         print('Main exception: ', MainException)
-        if MainException == -1:
-            print('GET reset time bitches')
-            reset()
-        # in case the wifi disconnects, try reconnecting
-        if not wlan.isconnected():
-            wlan = connectToWifi(ssid, passwd)
+        reset()
+
 
 
